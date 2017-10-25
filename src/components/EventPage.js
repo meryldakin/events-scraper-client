e8d2ee4a828fb319ba8b1547284d4b18b482d472
meryldakin <meryl.k.dakin@gmail.com>
2017-10-25 13:20:55 -0400

    remove save event button when event is already saved

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -8,24 +8,42 @@ import * as actions from "../actions/index";
 
 function EventPage(props) {
   const handleSave = () => {
-    console.log("event page props", props.current_user_id)
-    props.saveEvent(props.eventPage.id, props.current_user_id)
-  }
-  return <div>
-    <h1> {props.eventPage.name} </h1>
-    <p> {props.eventPage.description} </p>
-    <button onClick={handleSave}>Save!</button>
+    console.log("event page props", props.current_user_id);
+    props.saveEvent(props.eventPage.id, props.current_user_id);
+  };
 
-    <p><Link to="/events">Back to Events!</Link></p>
-  </div>;
+  const addButton = event => {
+    if (props.user_events.find(e => e.id === props.eventPage.id)) {
+      return <div>No button is here</div>;
+    } else {
+      return (
+        <button value={event.id} onClick={handleSave}>
+          Save Event!
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <h1> {props.eventPage.name} </h1>
+      <p> {props.eventPage.description} </p>
+
+      {addButton(props.eventPage)}
+
+      <p>
+        <Link to="/events">Back to Events!</Link>
+      </p>
+    </div>
+  );
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    current_user_id: state.users.current_user.id
-  }
+    current_user_id: state.users.current_user.id,
+    user_events: state.users.user_events
+  };
 }
-
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
